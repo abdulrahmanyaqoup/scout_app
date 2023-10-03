5bd5a36787e92dfd644ada217ff5113e1edcebba
ABDULRAHMANYAQOUP <83212579+abdulrahmanyaqoup@users.noreply.github.com>
2023-10-03 21:05:11 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Scout App',
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
+      home: const Main(),
+    );
+  }
+}
+
+class Main extends StatefulWidget {
+  const Main({
+    super.key,
+  });
+
+  @override
+  State<Main> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<Main> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
